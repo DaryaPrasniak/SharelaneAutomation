5e306cf06719c56c29d6a2f3422f505a70409bd5
Darya Prasniak <125681242+DaryaPrasniak@users.noreply.github.com>
2023-05-11 21:03:13 +0200

    Login and SignUp

---
+++ SharelaneAutomationTest/SharelaneAutomationTest/Tests/Login.cs
@@ -0,0 +1,48 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SharelaneAutomationTest.Pages;
+using System;
+
+namespace SharelaneAutomationTest
+{
+    internal class Login : BaseTest
+    {
+        [Test]
+        public void Test1() 
+        {
+            string email = "jennifer_huang@690.87.sharelane.com";
+            string password = "1111";
+            string welcomeText = "Hello Jennifer";
+
+            LoginPage.SetEmail(email);
+            LoginPage.SetPassword(password);
+            LoginPage.ClickLoginButton();
+
+            var helloText = ChromeDriver.FindElement(By.ClassName("user"));            
+            var logoutLink = ChromeDriver.FindElement(By.LinkText("Logout"));
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(welcomeText, helloText.Text);
+                Assert.IsTrue(logoutLink.Displayed);
+            });
+
+            Assert.IsTrue(LoginPage.CheckLogoutLink());
+
+        }
+
+        [Test]
+        public void Test2()
+        {
+            string email = "jennifer_huang@690.87.sharelane.com";
+            string password = "1111";
+
+            LoginPage.Login(email, password);
+            LoginPage.Logout();
+
+            var logoutText = ChromeDriver.FindElement(By.ClassName("confirmation_message"));
+            Assert.AreEqual("You've been logged out", logoutText.Text);
+        }
+    }
+}
